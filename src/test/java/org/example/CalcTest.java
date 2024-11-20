be24eb6bc9ffbc9ac286af3136d61a305c87540d
Pro100Dima1 <dima.sinkin@yandex.ru>
2024-11-20 13:47:08 +0300

    Add Test class and create tests

---
+++ src/test/java/org/example/CalcTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+    @BeforeAll
+    public static void startOfTests(){
+        System.out.println("Начало прогона тестов");
+    }
+    @Test
+    void
+
+
+    @AfterAll
+    public static void endOfTests(){
+        System.out.println("Конец прогона тестов");
+    }
+
+}
