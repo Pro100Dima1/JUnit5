ed75b8531e980fc4e6f8360100ee59cf4bbf76e5
Pro100Dima1 <dima.sinkin@yandex.ru>
2024-11-20 14:05:53 +0300

    Add Test Suite

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -1,8 +1,8 @@
 package org.example;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,8 +11,28 @@ class CalcTest {
     public static void startOfTests(){
         System.out.println("Начало прогона тестов");
     }
-    @Test
-    void
+
+    @ParameterizedTest(name = "№{index} - сложение {0} и {1}, ожидаемый результат {2}")
+    @CsvSource({"2, 5, 7", "2, 5, 8", "3, 10, 13"})
+    @DisplayName("Проверка работы сложения калькулятора")
+    @Tag("summ")
+    void testForSumm(int firstTerm, int secondTerm, int expected){
+        Calc calcSumm = new Calc();
+        int ressultSum = calcSumm.summation(firstTerm, secondTerm);
+        Assertions.assertEquals(expected, ressultSum, "Не работает");
+
+    }
+
+    @ParameterizedTest(name = "№{index} - вычитание {0} и {1}, ожидаемый результат {2}")
+    @CsvSource({"7, 2, 5", "2, 5, 8", "13, 10, 3"})
+    @DisplayName("Проверка работы вычитания калькулятора")
+    @Tag("sub")
+    void testForSubtraction(int firstTerm, int secondTerm, int expected){
+        Calc calcSub = new Calc();
+        int ressultSub = calcSub.subtraction(firstTerm, secondTerm);
+        Assertions.assertEquals(expected, ressultSub, "Не работает");
+
+    }
 
 
     @AfterAll
